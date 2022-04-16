05b409525a0e6d8affc90451ffa3722eec20f4c5
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-04-16 21:17:14 +0500

    save commit

--- src/Modules/Files/Controller.js
+++ src/Modules/Files/Controller.js
@@ -1,11 +1,22 @@
+import fs from 'fs'
 import FilesService from './Service'
 import SuccessResponse from '../../utils/SuccessResponse'
 import ErrorResponse from '../../utils/ErrorResponse'
 
 export default {
   async getFile(req, res) {
-    // console.log(req)
+    console.log(req.body)
     return res.send(new SuccessResponse(await FilesService.getFile('TEST')))
   },
-  async addFile(req, res) {},
+  async addFile(req, res) {
+    req.on('data', function (chunk) {
+      req.rawBody += chunk
+    })
+    req.on('end', function () {
+      console.log(req.headers)
+      fs.writeFileSync(`./static/${req.params.name}}`, req.rawBody, 'binary', err =>
+        console.log(err)
+      )
+    })
+  },
 }
