1484a7f194fa9ab8a366b2da58842949ca71858a
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-04-17 13:36:23 +0500

    import->export middlewares and Error in Files module index.js

--- src/Modules/Files/index.js
+++ src/Modules/Files/index.js
@@ -2,4 +2,9 @@ export { default as Controller } from './Controller.js'
 export { default as Service } from './Service.js'
 export { default as Model } from './Model.js'
 export { default as router } from './router.js'
+
+import * as middlewares from './middlewares/index.js'
+export { middlewares }
+
 export { default as migration } from './assets/migration.js'
+export { default as Errors } from './assets/Errors.js'
