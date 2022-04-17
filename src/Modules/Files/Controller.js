3b6f02396d4438ae3e25b79ece9a823bb764e71c
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-04-17 13:26:03 +0500

    realized create or edit file

--- src/Modules/Files/Controller.js
+++ src/Modules/Files/Controller.js
@@ -1,23 +1,18 @@
-import fs from 'fs'
 import FilesService from './Service.js'
-import SuccessResponse from '../../utils/SuccessResponse.js'
-import ErrorResponse from '../../utils/ErrorResponse.js'
-import { Model as File } from './index.js'
+import { SuccessResponse, ErrorResponse } from '../../utils/index.js'
 
 export default {
   async getFile(req, res) {
     console.log(req.body)
-    return res.send(new SuccessResponse(await FilesService.getFile('TEST')))
+    return res.json(new SuccessResponse(await FilesService.getOneByName('TEST')))
   },
   async addFile(req, res) {
     req.on('data', function (chunk) {
       req.rawBody += chunk
     })
-    req.on('end', function () {
-      console.log(req.headers)
-      fs.writeFileSync(`./static/${req.params.name}}`, req.rawBody, 'binary', err =>
-        console.log(err)
-      )
+    req.on('end', async function () {
+      const file = await FilesService.createOrEdit(req)
+      return res.json(new SuccessResponse({ file }))
     })
   },
 }
--- src/Modules/Files/Service.js
+++ src/Modules/Files/Service.js
@@ -1,5 +1,34 @@
+import fs from 'fs'
+import { Model as File } from './index.js'
+
 export default {
-  async getFile(fileName) {
-    return fileName
+  async getOneByName(fileName) {
+    return File.findOne({
+      where: { name: fileName },
+    })
+  },
+
+  async createOrEdit(req) {
+    const name = req.params.name
+    const path = `./static/${name}`
+    const type = req.headers['content-type']
+    const fileSize = req.headers['content-length']
+
+    const file =
+      (await this.getOneByName(name)) ||
+      (await this.createFile({
+        name: name,
+        path,
+        content_type: type,
+        size: fileSize,
+      }))
+
+    fs.writeFileSync(path, req.rawBody, 'binary', err => console.log(err))
+
+    return file
+  },
+
+  async createFile(file) {
+    return await File.create(file)
   },
 }
